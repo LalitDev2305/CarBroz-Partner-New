7d31a7d13d6429ae55238cfac8bb997b68d8d498
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-21 05:07:24 +0530

    feat: add application runtime module

---
+++ runtime/application/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKmpLibrary)
+    alias(libs.plugins.kover)
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "com.carbroz.runtime.application"
+        compileSdk = 37
+        minSdk = 24
+    }
+
+    jvm("desktop")
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":foundation:lifecycle"))
+            implementation(libs.kotlinx.coroutines.core)
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation(libs.kotlinx.coroutines.test)
+        }
+    }
+}
