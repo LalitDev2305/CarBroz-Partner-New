6986448e64d542aea376946693ac5576129acf40
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 02:03:55 +0530

    feat: initialize composition root from Android host

--- androidApp/src/main/kotlin/com/carbroz/partner/MainActivity.kt
+++ androidApp/src/main/kotlin/com/carbroz/partner/MainActivity.kt
@@ -3,12 +3,25 @@ package com.carbroz.partner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.carbroz.partner.composition.AppLifecycleBridge
 import com.carbroz.partner.composition.CarBrozApp
+import com.carbroz.partner.composition.initializeCarBrozDependencyInjection
 
 /** Android host entry point. Reusable application behavior belongs in KMP modules. */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initializeCarBrozDependencyInjection()
         setContent { CarBrozApp() }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        AppLifecycleBridge.moveToForeground()
+    }
+
+    override fun onStop() {
+        AppLifecycleBridge.moveToBackground()
+        super.onStop()
     }
 }
