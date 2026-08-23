8fca91a5b34a8a2e6be35de6471ffb513d816c7c
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-23 18:54:15 +0530

    feat: add security foundation module

---
+++ foundation/security/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKmpLibrary)
+    alias(libs.plugins.kover)
+}
+
+kotlin {
+    androidLibrary {
+        namespace = "com.carbroz.foundation.security"
+        compileSdk = 37
+        minSdk = 24
+        withHostTest {}
+    }
+
+    jvm("desktop")
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
