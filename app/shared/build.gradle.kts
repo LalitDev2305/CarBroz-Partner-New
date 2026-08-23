b3c605754f6769823afb665de66efa5a3bc2f9d4
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 01:06:58 +0530

    build: depend on dedicated adaptive foundation

--- app/shared/build.gradle.kts
+++ app/shared/build.gradle.kts
@@ -31,6 +31,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(project(":foundation:lifecycle"))
+            implementation(project(":foundation:adaptive"))
             implementation(project(":foundation:design-system"))
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
