6d9e0334a9c2e3fbd0d846e3acf7ce652cf589e7
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 05:13:15 +0530

    build: add SDUI runtime module

---
+++ runtime/sdui/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKmpLibrary)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.kover)
+}
+
+kotlin {
+    android {
+        namespace = "com.carbroz.runtime.sdui"
+        compileSdk = 36
+        minSdk = 24
+        withHostTest {}
+    }
+
+    jvm("desktop")
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.serialization.json)
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
