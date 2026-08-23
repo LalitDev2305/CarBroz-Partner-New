026ba886e5561f72ee1175d895147e9f768fcbb1
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 01:38:50 +0530

    refactor: use composition root from android host

--- androidApp/src/main/kotlin/com/carbroz/partner/MainActivity.kt
+++ androidApp/src/main/kotlin/com/carbroz/partner/MainActivity.kt
@@ -3,7 +3,7 @@ package com.carbroz.partner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.carbroz.partner.shared.CarBrozApp
+import com.carbroz.partner.composition.CarBrozApp
 
 /** Android host entry point. Reusable application behavior belongs in KMP modules. */
 class MainActivity : ComponentActivity() {
