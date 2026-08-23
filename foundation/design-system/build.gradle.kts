e0f0df4297f0ab0e392fe6d5f8114e03e79387b4
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-23 22:28:08 +0530

    build: add multiplatform design system foundation

---
+++ foundation/design-system/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKmpLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kover)
+}
+
+kotlin {
+    android {
+        namespace = "com.carbroz.foundation.designsystem"
+        compileSdk = 36
+        minSdk = 24
+    }
+
+    jvm("desktop")
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.ui)
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
