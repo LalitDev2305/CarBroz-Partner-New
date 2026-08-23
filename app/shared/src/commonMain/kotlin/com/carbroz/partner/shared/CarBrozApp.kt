8666e534516c58be8946ed2e94c9193aed6afcb5
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 01:07:08 +0530

    refactor: consume adaptive foundation from app shell

--- app/shared/src/commonMain/kotlin/com/carbroz/partner/shared/CarBrozApp.kt
+++ app/shared/src/commonMain/kotlin/com/carbroz/partner/shared/CarBrozApp.kt
@@ -6,7 +6,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.carbroz.foundation.designsystem.AdaptiveLayoutProvider
+import com.carbroz.foundation.adaptive.AdaptiveLayoutProvider
 import com.carbroz.foundation.designsystem.CarBrozTheme
 
 /**
