33d2eea2de9a3a950f5e08048227bebaa904d238
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-23 21:20:25 +0530

    feat: bridge iOS scene lifecycle to shared runtime

--- iosApp/CarBrozPartner/CarBrozPartnerApp.swift
+++ iosApp/CarBrozPartner/CarBrozPartnerApp.swift
@@ -1,10 +1,25 @@
 import SwiftUI
+import CarBrozShared
 
 @main
 struct CarBrozPartnerApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                AppLifecycleBridge.shared.moveToForeground()
+            case .background:
+                AppLifecycleBridge.shared.moveToBackground()
+            case .inactive:
+                break
+            @unknown default:
+                break
+            }
         }
     }
 }
