8faeb4b73b30003d6488bc7487b86094b1232795
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-23 20:35:54 +0530

    build: migrate session Android KMP target DSL

--- foundation/session/build.gradle.kts
+++ foundation/session/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 }
 
 kotlin {
-    androidLibrary {
+    android {
         namespace = "com.carbroz.foundation.session"
-        compileSdk = 37
+        compileSdk = 36
         minSdk = 24
         withHostTest {}
     }
