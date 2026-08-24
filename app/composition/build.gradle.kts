60d443f6200d87e84f675624e60e751eff886dfe
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 17:17:21 +0530

    feat(composition): depend on database foundation

--- app/composition/build.gradle.kts
+++ app/composition/build.gradle.kts
@@ -42,6 +42,7 @@ kotlin {
             implementation(project(":foundation:design-system"))
             implementation(project(":runtime:application"))
             implementation(project(":data:network"))
+            implementation(project(":data:database"))
             implementation(project(":data:secure-storage"))
             implementation(project.dependencies.platform(libs.koin.bom))
             implementation(libs.koin.core)
