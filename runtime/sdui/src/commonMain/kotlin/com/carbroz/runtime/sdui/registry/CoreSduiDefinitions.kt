47a734428d333205c104a92326dc41fa3fd725b8
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 12:40:02 +0530

    feat(sdui): register stack section and group definitions

--- runtime/sdui/src/commonMain/kotlin/com/carbroz/runtime/sdui/registry/CoreSduiDefinitions.kt
+++ runtime/sdui/src/commonMain/kotlin/com/carbroz/runtime/sdui/registry/CoreSduiDefinitions.kt
@@ -4,6 +4,8 @@ import com.carbroz.runtime.sdui.component.stack.StackComponentDefinition
 import com.carbroz.runtime.sdui.element.button.ButtonElementDefinition
 import com.carbroz.runtime.sdui.element.text.TextElementDefinition
 import com.carbroz.runtime.sdui.extension.SduiDefinition
+import com.carbroz.runtime.sdui.group.stack.StackGroupDefinition
+import com.carbroz.runtime.sdui.section.stack.StackSectionDefinition
 import com.carbroz.runtime.sdui.template.form.FormTemplateDefinition
 
 /**
@@ -14,6 +16,8 @@ object CoreSduiDefinitions {
     val all: List<SduiDefinition<*>> = listOf(
         FormTemplateDefinition,
         StackComponentDefinition,
+        StackSectionDefinition,
+        StackGroupDefinition,
         TextElementDefinition,
         ButtonElementDefinition,
     )
