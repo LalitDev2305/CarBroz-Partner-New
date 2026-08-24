a81815a344b53db74b2205f8c110a02d4827cfc5
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 17:14:10 +0530

    feat(database): add iOS Room builder provider

---
+++ data/database/src/iosMain/kotlin/com/carbroz/data/database/IosCarBrozDatabaseBuilderProvider.kt
@@ -0,0 +1,27 @@
+package com.carbroz.data.database
+
+import androidx.room3.Room
+import androidx.room3.RoomDatabase
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSApplicationSupportDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
+import platform.Foundation.URLByAppendingPathComponent
+
+/** iOS Room builder rooted in Application Support. */
+class IosCarBrozDatabaseBuilderProvider : CarBrozDatabaseBuilderProvider {
+    @OptIn(ExperimentalForeignApi::class)
+    override fun builder(): RoomDatabase.Builder<CarBrozDatabase> {
+        val fileManager = NSFileManager.defaultManager
+        val supportDirectory = fileManager.URLForDirectory(
+            directory = NSApplicationSupportDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = true,
+            error = null,
+        ) ?: error("Application Support directory is unavailable")
+        val databaseUrl = supportDirectory.URLByAppendingPathComponent(CarBrozDatabase.DATABASE_NAME)
+            ?: error("Database URL could not be resolved")
+        return Room.databaseBuilder<CarBrozDatabase>(name = databaseUrl.path ?: error("Database path is unavailable"))
+    }
+}
