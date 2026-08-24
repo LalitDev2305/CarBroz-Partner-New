9b6542d8bcb2adf778f4b25fd65ad75708ee4ce8
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 13:10:13 +0530

    feat(action): add core action definition bundle

---
+++ runtime/action/src/commonMain/kotlin/com/carbroz/runtime/action/CoreActionDefinitions.kt
@@ -0,0 +1,10 @@
+package com.carbroz.runtime.action
+
+import com.carbroz.runtime.action.request.RequestActionDefinition
+
+/** Registration bundle only; [ActionRegistry] remains the sole lookup mechanism. */
+object CoreActionDefinitions {
+    val all: List<ActionDefinition> = listOf(
+        RequestActionDefinition(),
+    )
+}
