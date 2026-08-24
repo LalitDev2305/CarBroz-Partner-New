97b1541c66a6b6d4edae71ace1b8f087b0ed0a79
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 17:11:46 +0530

    chore(database): acknowledge Room expect actual generation

--- data/database/build.gradle.kts
+++ data/database/build.gradle.kts
@@ -18,6 +18,10 @@ kotlin {
     iosArm64()
     iosSimulatorArm64()
 
+    compilerOptions {
+        freeCompilerArgs.add("-Xexpect-actual-classes")
+    }
+
     sourceSets {
         commonMain.dependencies {
             implementation(libs.kotlinx.coroutines.core)
