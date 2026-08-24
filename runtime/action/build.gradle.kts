e39c517257440cf770034e3ddf1e6bff0909808d
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 13:07:13 +0530

    feat(action): add multiplatform action runtime module

---
+++ runtime/action/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKmpLibrary)
+    alias(libs.plugins.kover)
+}
+
+kotlin {
+    android {
+        namespace = "com.carbroz.runtime.action"
+        compileSdk = 36
+        minSdk = 24
+        withHostTest {}
+    }
+
+    jvm("desktop")
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.serialization.json)
+            implementation(project(":runtime:binding"))
+            implementation(project(":runtime:form"))
+            implementation(project(":runtime:sdui"))
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
