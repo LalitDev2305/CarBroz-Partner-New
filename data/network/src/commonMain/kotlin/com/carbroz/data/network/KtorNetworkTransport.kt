3ca0384706324df1c911abae0a58553c73fa017e
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 15:33:30 +0530

    fix(network): retain normalized transport diagnostics

--- data/network/src/commonMain/kotlin/com/carbroz/data/network/KtorNetworkTransport.kt
+++ data/network/src/commonMain/kotlin/com/carbroz/data/network/KtorNetworkTransport.kt
@@ -63,8 +63,12 @@ class KtorNetworkTransport(
         throw error
     } catch (_: HttpRequestTimeoutException) {
         NetworkResult.Failure(NetworkFailure.Timeout)
-    } catch (_: Throwable) {
-        NetworkResult.Failure(NetworkFailure.Transport())
+    } catch (error: Throwable) {
+        NetworkResult.Failure(
+            NetworkFailure.Transport(
+                reason = error.message ?: error::class.simpleName,
+            ),
+        )
     }
 
     fun close() {
