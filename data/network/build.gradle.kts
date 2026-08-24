f7000b32690f6eb209a267bd28f749e04de0318f
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-24 15:07:09 +0530

    refactor(network): remove runtime dependency leak

--- data/network/build.gradle.kts
+++ data/network/build.gradle.kts
@@ -19,11 +19,6 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.kotlinx.serialization.json)
-            implementation(project(":runtime:action"))
-            // Temporary compile-time requirement: PreparedAction.Request exposes RequestMethod,
-            // which is owned by runtime:sdui. This dependency will disappear when action/network
-            // mapping moves to the orchestration boundary.
-            implementation(project(":runtime:sdui"))
         }
         commonTest.dependencies {
             implementation(kotlin("test"))
