00d954d03898b1fed590e28d3949ed2a2814b196
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-25 16:18:50 +0530

    Fix SystemClock actual method declarations

--- foundation/time/src/iosMain/kotlin/com/carbroz/foundation/time/SystemClock.ios.kt
+++ foundation/time/src/iosMain/kotlin/com/carbroz/foundation/time/SystemClock.ios.kt
@@ -4,6 +4,6 @@ import platform.Foundation.NSDate
 import platform.Foundation.timeIntervalSince1970
 
 actual object SystemClock : Clock {
-    override fun nowEpochMilliseconds(): Long =
+    actual override fun nowEpochMilliseconds(): Long =
         (NSDate().timeIntervalSince1970 * 1_000.0).toLong()
 }
