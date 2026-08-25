fabd6ec64d9ef769eb156aab73610c01506eec00
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-25 20:18:41 +0530

    Add Phase 14 observability module

---
+++ foundation/observability/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKmpLibrary)
+    alias(libs.plugins.kover)
+}
+
+kotlin {
+    android {
+        namespace = "com.carbroz.foundation.observability"
+        compileSdk = 36
+        minSdk = 24
+        withHostTest {}
+    }
+
+    jvm("desktop")
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
