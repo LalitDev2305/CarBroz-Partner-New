4bbdb9efd87f1beda34e8e083bad80add26aef9b
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-25 13:09:27 +0530

    Add Phase 13 background execution module

---
+++ capabilities/background/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKmpLibrary)
+    alias(libs.plugins.kover)
+}
+
+kotlin {
+    android {
+        namespace = "com.carbroz.capabilities.background"
+        compileSdk = 36
+        minSdk = 24
+        withHostTest {}
+    }
+
+    jvm("desktop")
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+        }
+        androidMain.dependencies {
+            implementation(libs.androidx.work.runtime)
+            implementation(project.dependencies.platform(libs.koin.bom))
+            implementation(libs.koin.core)
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation(libs.kotlinx.coroutines.test)
+        }
+    }
+}
