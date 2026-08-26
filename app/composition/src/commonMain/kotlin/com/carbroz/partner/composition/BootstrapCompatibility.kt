afc3e2c621fa6dc9cb37ba8f3476addfdb9d31bf
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-26 18:40:49 +0530

    refactor(startup): bridge composition during dynamic migration

---
+++ app/composition/src/commonMain/kotlin/com/carbroz/partner/composition/BootstrapCompatibility.kt
@@ -0,0 +1,5 @@
+package com.carbroz.partner.composition
+
+/** Temporary source-compatibility alias while DI composition is migrated to the generic name. */
+@Deprecated("Use BootstrapDestinationStore", ReplaceWith("BootstrapDestinationStore"))
+typealias BootstrapRouteStore = BootstrapDestinationStore
