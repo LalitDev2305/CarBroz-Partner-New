343573079d4058521abfe4ad153f7b5b7a3f68dd
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-08-27 10:37:51 +0530

    build(runtime): align application Android compile SDK

--- runtime/application/build.gradle.kts
+++ runtime/application/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 kotlin {
     android {
         namespace = "com.carbroz.runtime.application"
-        compileSdk = 36
+        compileSdk = 37
         minSdk = 24
         withHostTest {}
     }
