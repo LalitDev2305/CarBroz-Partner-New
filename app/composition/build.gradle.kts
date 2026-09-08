9b5c801ec6c22b5e6b56b8b113472b3df0029ade
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-08 06:36:50 +0530

    build(composition): wire canonical bootstrap module

--- app/composition/build.gradle.kts
+++ app/composition/build.gradle.kts
@@ -54,7 +54,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             api(foundationDependency("configuration"))
-            implementation(project(":app:startup"))
+            implementation(project(":data:bootstrap"))
             implementation(project(":feature:splash"))
             implementation(project(":feature:dynamic"))
             implementation(foundationDependency("lifecycle"))
