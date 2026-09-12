cd21c3341af36d3b4f474ebe20d8fa8eab1f2fe4
Lalit Naiya <developer.lalit.naiya@gmail.com>
2026-09-12 20:59:29 +0530

    fix(dynamic): compile generic overlay rendering

--- feature/dynamic/src/commonMain/kotlin/com/carbroz/feature/dynamic/DynamicScreen.kt
+++ feature/dynamic/src/commonMain/kotlin/com/carbroz/feature/dynamic/DynamicScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -23,6 +24,7 @@ import com.carbroz.sdui.value.SduiExecutionContext
 import com.carbroz.sdui.value.SduiValueResolution
 import com.carbroz.sdui.value.SduiValueResolver
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DynamicScreen(
     state: DynamicScreenState,
